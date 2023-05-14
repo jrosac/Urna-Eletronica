856fd60b3e3ac5f008c1fe253ec5f7d0ae665c2a
Pablo <pablooficial@academico.ufs.br>
2023-05-14 04:22:22 -0300

    nova interface da urna e posters do filme

--- src/main/java/visao/TelaUrna.java
+++ src/main/java/visao/TelaUrna.java
@@ -1,4 +1,5 @@
 package visao;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,15 +7,20 @@ import java.io.*;
 
 public class TelaUrna extends JFrame implements ActionListener {
 
-    private JButton[] botoesCandidato;
-    private JLabel[] lblVotosCandidato;
-    private int[] votosCandidato;
+    private String[] nomesCandidato;
+    private int[] numerosCandidato;
     private int numCandidatos;
-
+    private String voto;
+    private JLabel labelDisplay;
+    private JButton[] botoesNumeros;
+    private JButton botaoBranco;
+    private JButton botaoCorrige;
+    private JButton botaoConfirma;
+    
     public TelaUrna(String nomeArquivo) {
         super("Urna Eletronica");
-
-        // Lê o nome dos candidatos no txt
+    
+        // Lê o nome e o número dos candidatos no arquivo
         try {
             BufferedReader br = new BufferedReader(new FileReader(nomeArquivo));
             String linha;
@@ -30,26 +36,67 @@ public class TelaUrna extends JFrame implements ActionListener {
             }
             br.close();
 
-            //Botões de votação
-            botoesCandidato = new JButton[numCandidatos];
-            for (int i = 0; i < numCandidatos; i++) {
-                botoesCandidato[i] = new JButton(nomesCandidato[i]);
-                botoesCandidato[i].addActionListener(this);
+            // Cria o teclado da urna
+            JPanel teclado = new JPanel(null);
+            teclado.setPreferredSize(new Dimension(270, 400));
+            botoesNumeros = new JButton[10];
+            int x = 10;
+            int y = 10;
+            for (int i = 1; i <= 9; i++) {
+                botoesNumeros[i] = new JButton(Integer.toString(i));
+                botoesNumeros[i].setBounds(x, y, 80, 80);
+                botoesNumeros[i].addActionListener(this);
+                teclado.add(botoesNumeros[i]);
+                x += 90;
+                if (i % 3 == 0) {
+                    x = 10;
+                    y += 90;
+                }
             }
 
-            // Adiciona os botões de votação ao painel principal
-            JPanel pnlVotacao = new JPanel();
-            pnlVotacao.setLayout(new GridLayout(numCandidatos, 1));
-            for (int i = 0; i < numCandidatos; i++) {
-                pnlVotacao.add(botoesCandidato[i]);
-            }
+            // Botão 0
+            botoesNumeros[0] = new JButton("0");
+            botoesNumeros[0].setBounds(100, 280, 80, 80);
+            botoesNumeros[0].addActionListener(this);
+            teclado.add(botoesNumeros[0]);
 
-            // Adiciona os painéis ao frame
-            getContentPane().setLayout(new BorderLayout());
-            getContentPane().add(pnlVotacao, BorderLayout.CENTER);
+            // Botão Branco
+            botaoBranco = new JButton("BRANCO");
+            botaoBranco.setBounds(10, 370, 100, 60);
+            botaoBranco.addActionListener(this);
+            teclado.add(botaoBranco);
 
-            // Configura o tamanho do frame
-            setSize(400, 200);
+            // Botão Corrige
+            botaoCorrige = new JButton("CORRIGE");
+            botaoCorrige.setBounds(100, 370, 100, 60);
+            botaoCorrige.addActionListener(this);
+            teclado.add(botaoCorrige);
+
+            // Botão Confirma
+            botaoConfirma = new JButton("CONFIRMA");
+            botaoConfirma.setBounds(190, 370, 100, 60);
+            botaoConfirma.addActionListener(this);
+            teclado.add(botaoConfirma);
+
+            add(teclado, BorderLayout.CENTER);
+            setSize(800, 600);
+
+            // Cria o painel que irá conter o teclado e o display
+JPanel painelUrna = new JPanel(new BorderLayout());
+add(painelUrna, BorderLayout.CENTER);
+
+// Adiciona o teclado ao painel da urna
+painelUrna.add(teclado, BorderLayout.CENTER);
+
+// Cria o display e o adiciona ao painel da urna
+labelDisplay = new JLabel();
+labelDisplay.setBackground(Color.WHITE);
+labelDisplay.setOpaque(true);
+labelDisplay.setHorizontalAlignment(JLabel.CENTER);
+labelDisplay.setFont(new Font("Arial", Font.BOLD, 48));
+painelUrna.add(labelDisplay, BorderLayout.NORTH);
+
+
 
         } catch (IOException e) {
             System.err.println("Erro ao ler arquivo " + nomeArquivo + ": " + e.getMessage());
@@ -57,22 +104,28 @@ public class TelaUrna extends JFrame implements ActionListener {
         }
     }
 
-    public void actionPerformed(ActionEvent evt) {
-        // Verifica qual botão foi clicado
-        Object source = evt.getSource();
-
-        // Incrementa o contador de votos do candidato correspondente
-        for (int i = 0; i < numCandidatos; i++) {
-            if (source == botoesCandidato[i]) {
-                votosCandidato[i]++;
-                lblVotosCandidato[i].setText(botoesCandidato[i].getText() + ": " + votosCandidato[i]);
-                break;
-            }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+    
+        if (source == botaoBranco) {
+            voto = "BRANCO";
+        } else if (source == botaoCorrige) {
+            voto = "";
+            labelDisplay.setText(voto);
+        } else if (source == botaoConfirma) {
+            // Confirma o voto
+            // ...
+        } else {
+            // Um dos botões numéricos foi pressionado
+            JButton button = (JButton) source;
+            voto += button.getText();
+            labelDisplay.setText(voto);
         }
     }
-
+    
     public static void main(String[] args) {
         TelaUrna urna = new TelaUrna("src/main/resources/candidatos.txt");
         urna.setVisible(true);
     }
-}
+ }
